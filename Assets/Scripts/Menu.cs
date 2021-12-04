814df391d1f924904d2bbe572b11ed68df029a04
fabientolsy <2031900@cgmatane.qc.ca>
2021-12-04 09:28:52 -0500

    gestion des dossiers

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject fenetreCredits;
+    public Text textTitre;
+
+    private void Start()
+    {
+        textTitre.text = "Free Guy !";
+    }
+
+    public void BoutonJouer()
+    {
+        Debug.Log("Jouer !");
+        SceneManager.LoadScene("Plateau1");
+    }
+
+    public void BoutonQuitter()
+    {
+        Debug.Log("Quitter !");
+        Application.Quit();
+    }
+}
