5f69a5334f14f02376485f5421d172c449308f77
Jules <julesbrulon@orange.fr>
2021-12-04 20:49:28 -0500

    menu

--- Assets/Scripts/MenuGameOver.cs
+++ Assets/Scripts/MenuGameOver.cs
@@ -4,27 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class Menu : MonoBehaviour
+public class MenuGameOver : MonoBehaviour
 {
-    public Text textTitre;
-
-    private void Start()
-    {
-        textTitre.text = ("Free Guy !");
-    }
-
-    public void BoutonJouer()
-    {
-        Debug.Log("Jouer !");
-        SceneManager.LoadScene("Plateau1");
-    }
-
-    public void BoutonQuitter()
-    {
-        Debug.Log("Quitter !");
-        Application.Quit();
-    }
-
     public void BoutonMenu()
     {
         Debug.Log("Menu !");
